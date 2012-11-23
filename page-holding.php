aced7346e17f4a59a424e9ce67ad2d6b4aeebd3f
Henry Proudlove <henry.proudlove@gmail.com>
2012-11-23 19:08:02 +0000

    added holding page

---
+++ page-holding.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Template Name: Holding Page
+ * Description: Holding Page
+ *
+ * @package WordPress
+ * @subpackage themename
+ */
+?>
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+	<title>Plant For Peace</title>
+	<style type="text/css">
+		div{
+			background: url('<?php echo get_template_directory_uri(); ?>/images/holding-page.jpg') no-repeat top center;
+			width: 100%;
+			height: 1697px;
+		}
+		body{
+			width: 100%;
+		}
+	</style>
+</head>
+<body>
+	<div></div>
+</body>
+</html>
